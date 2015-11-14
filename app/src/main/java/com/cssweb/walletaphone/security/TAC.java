7de103b13d657a8a1828dd7cf6fc4123d76b0bb1
anders chen <chenhf2010@qq.com>
2015-11-14 22:27:28 +0800

    增加银行卡加解密算法

---
+++ app/src/main/java/com/cssweb/walletaphone/security/TAC.java
@@ -0,0 +1,8 @@
+package com.cssweb.walletaphone.security;
+
+/**
+ * Created by chenh on 2015/11/14.
+ */
+public class TAC {
+    //http://wenku.baidu.com/link?url=WCEjLeh7jUzdYAuZIh74UcnIU5dtfjWq3faHZjvYIwTSw2n1jj-_IhVgnyJEmoqYD92NA8d8p0G427JqEiB-L3o4iUNTFmCdbEqxELJyNLO
+}
